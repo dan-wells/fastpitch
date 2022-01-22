1b09a75fb7bf4355c3603eaf0801dfcab095cfe3
Dan Wells <danwells396@gmail.com>
2022-01-22 14:50:14 +0000

    Remove unnecessary TensorBoard hparams logging
    This doesn't seem to display anything useful in TensorBoard, and writes
    to an extra events file which clutters up log directories, so it seems
    nicer just to get rid of it.

--- common/tb_dllogger.py
+++ common/tb_dllogger.py
@@ -178,11 +178,6 @@ def parameters(data, verbosity=0, tb_subset=None):
     for k,v in data.items():
         dllogger.log(step="PARAMETER", data={k:v}, verbosity=verbosity)
 
-    if tb_subset is not None and tb_loggers[tb_subset].enabled:
-        tb_data = {k:v for k,v in data.items()
-                   if type(v) in (str, bool, int, float)}
-        tb_loggers[tb_subset].summary_writer.add_hparams(tb_data, {})
-
 
 def flush():
     dllogger.flush()
